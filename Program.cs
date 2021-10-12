5cfc7abcbb8c87279f9206adfe2a8ea04517fece
sarah sida <nestsida@gmail.com>
2021-10-11 20:04:40 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulation_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+            Console.WriteLine("How much do you want to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+            
+            ba.Deposit(amountToDeposit);
+            double userBalance = ba.GetBalance();
+
+        }
+    }
+}
